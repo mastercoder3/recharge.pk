c7e91540f86881c8bca6900f0b32a667160d1de4
Muhammad Junaid Hassan <muhammadjunaidhassan1@gmail.com>
2019-11-29 13:59:26 +0400

    admin exit added

--- src/app/admin/home/home.page.ts
+++ src/app/admin/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
+import { MenuController, NavController, Platform } from '@ionic/angular';
 import { ApiService } from 'src/app/services/api.service';
 import { map } from 'rxjs/operators';
 import { HelperService } from 'src/app/services/helper.service';
@@ -18,7 +18,7 @@ export class HomePage implements OnInit {
   topups=[];
   rechrage=[];
 
-  constructor(private menu: MenuController, private navigation: NavController, private api: ApiService, private helper: HelperService, private fcm: FcmService, private localNotifications: LocalNotifications) { 
+  constructor( private navigation: NavController, private api: ApiService, private helper: HelperService, private fcm: FcmService, private localNotifications: LocalNotifications, private platform: Platform) { 
     this.fcm.getToken(localStorage.getItem('uid'));
 
     this.fcm.listenToNotifications().pipe(
@@ -33,6 +33,18 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
+
+    this.platform.backButton.subscribe( () =>{
+      if(this.helper.active === false){
+       let func = () =>{
+         this.helper.active = false;
+         navigator['app'].exitApp();
+       };
+ 
+       this.helper.presentAlertMessage('Exit PayPak','Do you want to exit?','No','Yes', func);
+      }
+     });
+
     this.api.getAllPendingTopUpRequests()
     .pipe(map(actions => actions.map(a =>{
       const data = a.payload.doc.data();
@@ -60,25 +72,6 @@ export class HomePage implements OnInit {
 
   charge(item){
     this.helper.presentModal('bal',item);    
-  }
-
-  openMenu(){
-    this.menu.open('menu-content00');
-  }
-
-  navigateUpdation(){
-    this.menu.close('menu-content00');
-    this.navigation.navigateForward('admin/users');
-  }
-
-  logout(){
-    this.menu.close('menu-content00');
-    localStorage.clear();    
-    this.navigation.navigateRoot('login');
-  }
-
-  navigateHome(){
-    this.menu.close('menu-content00');
   }
 
   changeTab(val){
